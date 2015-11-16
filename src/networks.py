8dbe1c5f1ed34926b5d17d4b0fab62442fdf2c69
Ecucoin <ecucoin@gamecredits.net>
2015-11-16 19:45:36 +0100

    initial commit

---
+++ src/networks.py
@@ -0,0 +1,24 @@
+# Main network and testnet3 definitions
+
+params = {
+    'bitcoin_main': {
+        'pubkey_address': 0,
+        'script_address': 5,
+        'genesis_hash': '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+    },
+    'bitcoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
+    },
+    'gamecredits_main': {
+        'pubkey_address': 38,
+        'script_address': 5,
+        'genesis_hash': '91ec5f25ee9a0ffa1af7d4da4db9a552228dd2dc77cdb15b738be4e1f55f30ee'
+    },
+    'gamecredits_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': 'f5ae71e26c74beacc88382716aced69cddf3dffff24f384e1808905e0188f68f'
+    }
+}
